d4d937289ea094f46d57c139c65e12e04574c1bb
LukasKmci <kl4641@mci4me.at>
2025-03-24 23:08:41 +0100

    Rename testing-git to testing-git.txt

--- Aufgabe 3/main.py
+++ Aufgabe 3/main.py
@@ -2,13 +2,10 @@ from datetime import date
 from my_package import build_person, build_experiment
 
 def main():
-    # Supervisor erstellen
-    supervisor = build_person("Max", "Mustermann", "male", 35)
+    supervisor = build_person("Lukas", "Koehler", "male", 19)
 
-    # Subject erstellen
-    subject = build_person("Anna", "Schmidt", "female", 28)
+    subject = build_person("Marven", "Otto", "female", 66)
 
-    # Experiment erstellen
     experiment = build_experiment(
         experiment_name="Ausdauerleistung Herzfrequenz",
         date=str(date.today()),
@@ -16,7 +13,6 @@ def main():
         subject=subject
     )
 
-    # Ausgabe des Experiment-Dictionaries
     print(experiment)
 
 if __name__ == "__main__":
